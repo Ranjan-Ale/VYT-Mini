b30e360d59873b8207d63d4898363167ffd2589c
Ranjan <insookmess@gmail.com>
2026-08-09 21:34:22 +0545

    added cart feature

--- routes/index.js
+++ routes/index.js
@@ -2,15 +2,33 @@ const express = require("express")
 const router = express.Router()
 const isLoggedIn = require("../middlewares/isLoggedIn");
 const productModel = require("../models/product-model");
+const userModel = require("../models/user-model");
 
 router.get("/", (req,res)=>{
     let error = req.flash("error");
-    res.render("index", {error});
+    res.render("index", {error, loggedin: false});
 })
 
 router.get("/shop", isLoggedIn,async function (req,res){
     let products = await productModel.find()
-    res.render("shop", {products});
+    let success = req.flash("success")
+    res.render("shop", {products, success});
 })
+
+router.get("/addtocart/:id", isLoggedIn, async function (req,res){
+    let user = await userModel.findOne({email: req.user.email});
+    user.cart.push(req.params.id);
+    await user.save();
+    req.flash("success", "added to cart")
+    res.redirect("/shop")    
+})
+
+router.get("/cart", isLoggedIn, async function (req,res){
+    let user = await userModel.findOne({ email: req.user.email}).populate('cart');
+
+    const bill = Number(user.cart[0].price+20)- Number(user.cart[0].discount);
+    res.render('cart', {user, bill})
+})
+
 
 module.exports = router
